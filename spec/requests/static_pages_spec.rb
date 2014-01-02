e55f9444bae83bb9239727ccdb5922aacf26c5c4
Kai Teoh <kai@ink-cafe.com>
2014-01-02 13:50:50 -0600

    Add basic dynamic code, update Gemfile to include Guard and Spork.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,15 +2,21 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let (:base_title){"Ruby on Rails Tutorial Sample App"}
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
-    it "should have the right title" do
+    it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
   describe "Help page" do
@@ -21,7 +27,7 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
   end
   describe "About page" do
@@ -32,8 +38,20 @@ describe "Static pages" do
     end
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
+  end
+
+  describe "Contact" do
+
+  	it "should have the content 'Contact Us'" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_content('Contact Us')
+  	end
+  	it "should have the right title" do
+  		visit '/static_pages/contact'
+  		expect(page).to have_title("#{base_title} | Contact Us")
+  	end
   end
 
 end
