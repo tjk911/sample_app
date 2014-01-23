5dad3764d5abe42bf9cedce30d79fd9385dd3022
Kai Teoh <kai@ink-cafe.com>
2014-01-23 15:06:15 -0600

    Create relationships model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,10 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :password, length: { minimum: 6 }
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed 
+  has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
@@ -19,6 +23,18 @@ class User < ActiveRecord::Base
   def feed
     #preliminary stuff
     Micropost.where("user_id = ?", id)
+  end
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   private
